615bafa63e89a208f3e3883ad72beed8aaae8cb3
Isaia Tonini <isaia.tonini@gmail.com>
2023-11-14 14:48:11 +0100

    pages main view template

---
+++ src/pages/MainView.svelte
@@ -0,0 +1,6 @@
+<main class="min-h-screen max-w-full bg-gray-100 dark:bg-gray-600 flex flex-col items-center">
+    <slot name="navbar"/>
+    <div class="flex justify-center items-center h-auto flex-grow w-full">
+        <slot name="content"/>
+    </div>
+</main>
