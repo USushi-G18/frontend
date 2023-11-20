726359be50f3c16f4ac2feb996ab3cc23f5d9a88
Isaia Tonini <isaia.tonini@gmail.com>
2023-11-20 12:47:20 +0100

    added a local menu to show the correct price and list of plates in the /restaurant route

---
+++ src/store/selected_menu.ts
@@ -0,0 +1,12 @@
+import { writable, type Writable } from 'svelte/store';
+import { Menu } from '../models/menu';
+
+// Inizializza il carrello di partenza con il valore corrente del carrello in localStorage
+const initialMenu: Menu = localStorage.getItem('menu') as Menu || undefined;
+
+export const selectedMenu: Writable<Menu> | undefined = writable(initialMenu);
+
+// Ogni volta che il carrello cambia, aggiorna localStorage
+selectedMenu.subscribe(value => {
+  localStorage.setItem('menu', value);
+});
