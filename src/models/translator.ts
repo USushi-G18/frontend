9e2dfe73ac7f68f35aae4a868b3e60d3c7bbe6db
Isaia Tonini <isaia.tonini@gmail.com>
2023-12-03 16:27:48 +0100

    added a translator mainly used for type's keys

---
+++ src/models/translator.ts
@@ -0,0 +1,18 @@
+
+export const translator: Translator = {
+	"name": "nome",
+	"price": "prezzo",
+	"ingredients": "ingredienti",
+	"category": "categoria",
+	"menu": "menu",
+	"img": "immagine",
+	"description": "descrizione",
+	"limit": "limite",
+	"Carte": "Alla carta",
+	"Lunch": "Pranzo",
+	"Dinner": "Cena",
+}
+
+type Translator = {
+	[key: string]: string;
+};
