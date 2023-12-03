0f1075d56f7034d487db6c1b19d11b36b4f6545b
Isaia Tonini <isaia.tonini@gmail.com>
2023-12-03 16:52:06 +0100

    titles of login tabs (in user selection)

---
+++ src/components/login/LoginUsers.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+	import { UserSettingsSolid, WalletSolid, UsersSolid } from 'flowbite-svelte-icons';
+	export let user = '';
+</script>
+
+<div class="flex items-center justify-center gap-2 px-3 text-gray-800 dark:text-gray-400">
+	{#if user == "admin"}
+		<UserSettingsSolid size="md"tabindex="-1" class="outline-none"/>
+		<span>Admin</span>
+	{:else if user == "kitchen"}
+		<UsersSolid size="md" tabindex="-1"  class="outline-none"/>
+		<span>Dipendenti</span>
+	{:else if user == "restaurant"}
+		<WalletSolid size="md" tabindex="-1"  class="outline-none"/>
+		<span>Clienti</span>
+	{/if}
+</div>
