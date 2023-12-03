851b62cc06ac59c38fe5270a90f43e135497a98f
Isaia Tonini <isaia.tonini@gmail.com>
2023-12-03 16:47:08 +0100

    card used to trigger password update form

---
+++ src/components/admin/PwCard.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+	import { Card, Heading, Button} from 'flowbite-svelte';
+	import PwInput from '../PwInput.svelte';
+	import PwModifier from './PwModifier.svelte';
+
+	export let user:string;
+	let openModal:boolean;
+</script>
+
+<Card>
+	<Heading tag="h4" class="mb-4">{user}</Heading>
+	<PwInput value="passwordPlaceholder" disabled/>
+	<Button class="mt-5" on:click={() => openModal = true}>Modifica</Button>
+	<PwModifier {user} bind:openModal={openModal}/>
+</Card>
