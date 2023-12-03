932f6348979f07128de79c6e8fdb1cc4818f13df
Isaia Tonini <isaia.tonini@gmail.com>
2023-12-03 16:51:11 +0100

    "modify" button triggering a form

---
+++ src/components/admin/ItemModifyBtn.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+	import { Tooltip } from "flowbite-svelte";
+	import { FilePenOutline } from "flowbite-svelte-icons";
+	import ItemModifier from "./ItemModifier.svelte";
+
+	export let item: any;
+	let openModal = false;
+</script>
+
+<FilePenOutline on:click={() => {openModal = true;}}/>
+<Tooltip
+	shadow={false}
+	class="bg-sky-300 dark:bg-sky-200 text-sky-900"
+	>Modifica</Tooltip
+>
+<ItemModifier bind:openModal {item} />
