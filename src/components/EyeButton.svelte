47682d652e92d081d3a0f6ac8309fafb2e387e52
Isaia Tonini <isaia.tonini@gmail.com>
2023-12-03 16:39:33 +0100

    dynamic eye icon in password fields

---
+++ src/components/EyeButton.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+	export let visible = false;
+	import { EyeOutline, EyeSlashOutline } from 'flowbite-svelte-icons';
+
+	export let tabindex = 0;
+</script>
+{#if visible}
+	<EyeOutline {tabindex}/>
+{:else}
+	<EyeSlashOutline {tabindex}/>
+{/if}
