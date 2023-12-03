e69fc05ac19069d31982e6c2a7f6ab24851f9044
Isaia Tonini <isaia.tonini@gmail.com>
2023-12-03 16:58:04 +0100

    modal displaying more detailed plate's infos

---
+++ src/components/restaurant/PlateInfos.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+	import type { Plate } from "../../models/plate";
+    import { Img, Modal } from "flowbite-svelte";
+
+	export let openModal: boolean;
+	export let plate: Plate;
+</script>
+
+<Modal bind:open={openModal} autoclose outsideclose title={plate.name}>
+	<div class="">
+		<Img class="rounded-lg w-72 float-right aspect-video" src={plate.img} alt="{plate.name}"/>
+		<p><b class="dark:text-white text-gray-700">Descrizione</b><br> {"\n\n" + plate.description}</p><br>
+		<p><b class="dark:text-white text-gray-700">Categoria</b><br> {plate.category}</p><br>
+		<p><b class="dark:text-white text-gray-700">Ingredienti</b><br> {plate.ingredients}</p><br>
+		<p><b class="dark:text-white text-gray-700">Prezzo</b><br> {plate.price + "€"}</p><br>
+		<p><b class="dark:text-white text-gray-700">Menu</b><br> {plate.menu}</p><br>
+		<p><b class="dark:text-white text-gray-700">Limite</b><br> {plate.limit}</p><br>
+	</div>
+</Modal>
