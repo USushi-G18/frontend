691094a711477eb8e640da1abe85d6ea6762f8fc
Isaia Tonini <isaia.tonini@gmail.com>
2023-12-03 16:37:23 +0100

    main login page with user selection

---
+++ src/pages/login/Login.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+	import { TabItem, Tabs } from 'flowbite-svelte';
+    import LoginContent from '../../components/login/LoginContent.svelte';
+    import LoginUsers from '../../components/login/LoginUsers.svelte';
+</script>
+
+<form class="dark:bg-gray-900 bg-gray-300 rounded-lg w-[80%] sm:w-auto">
+	<Tabs style="underline" contentClass="p-4 bg-gray-200 dark:bg-gray-800 rounded-b-lg">
+		<TabItem open>
+			<LoginUsers user="admin" slot="title"/>
+			<LoginContent user="admin" />
+		</TabItem>
+		<TabItem>
+			<LoginUsers user="kitchen" slot="title"/>
+			<LoginContent user="kitchen"  />
+		</TabItem>
+		<TabItem>
+			<LoginUsers user="restaurant" slot="title"/>
+			<LoginContent user="restaurant"/>
+		</TabItem>
+	</Tabs>	
+</form>
