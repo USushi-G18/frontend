8969a1d445f715279a6861cc91966f0d1a08ae82
Isaia Tonini <isaia.tonini@gmail.com>
2023-12-03 17:00:13 +0100

    grid layout for plate cards

---
+++ src/components/restaurant/PlatesDisplay.svelte
@@ -0,0 +1,25 @@
+<script lang="ts">
+	import type { Plate } from "../../models/plate";
+
+	import PlateCard from "./PlateCard.svelte";
+
+	export let plates: Plate[] | undefined;
+</script>
+
+<div class="plates my-7 w-full items-center justify-center">
+	{#if plates}
+		{#each plates as plate}
+			<PlateCard
+				{plate}
+			/>
+		{/each}
+	{/if}
+</div>
+
+<style>
+	.plates {
+		display: grid;
+		grid-gap: 1.25rem;
+		grid-template-columns: repeat(auto-fit, minmax(250px, 1fr));
+	}
+</style>
