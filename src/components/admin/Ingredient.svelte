560ad60ece57b5aa164de6f6d38aa57a196aa47f
Isaia Tonini <isaia.tonini@gmail.com>
2024-01-08 10:02:26 +0100

    added ingredient checkbox

---
+++ src/components/admin/Ingredient.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+    import { Checkbox, Label } from "flowbite-svelte";
+	import { createEventDispatcher } from "svelte";
+
+	export let checked = false;
+	export let content: string;
+
+	const dispatch = createEventDispatcher();
+
+	function handleClick() {
+		dispatch("click");
+	}
+</script>
+
+
+<Label>
+	<Checkbox name={content} bind:checked  on:click={handleClick}>{content}</Checkbox>
+</Label>
